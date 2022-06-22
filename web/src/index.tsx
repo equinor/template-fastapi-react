2cfa523e2e1df8ebe0fa3b003fc6936d1bebaa13
Olava Faksdal <38139899+olavis@users.noreply.github.com>
2022-06-22 14:11:13 +0200

    feat: create-react-app with typescript and equinor icons

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,36 +1,12 @@
-import App from './App'
 import React from 'react'
-import ReactDOM from 'react-dom'
-import { AuthContext, AuthProvider, TAuthConfig } from 'react-oauth2-code-pkce'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-const authEnabled = process.env.REACT_APP_AUTH === '1'
-
-const authConfig = {
-  clientId: process.env.REACT_APP_AUTH_CLIENT_ID || '',
-  authorizationEndpoint: process.env.REACT_APP_AUTH_ENDPOINT || '',
-  tokenEndpoint: process.env.REACT_APP_TOKEN_ENDPOINT || '',
-  scope: process.env.REACT_APP_AUTH_SCOPE || '',
-  redirectUri: process.env.REACT_APP_AUTH_REDIRECT_URI || '',
-  logoutEndpoint: process.env.REACT_APP_LOGOUT_ENDPOINT || '',
-  preLogin: () =>
-    localStorage.setItem(
-      'preLoginPath',
-      `${window.location.pathname}${window.location.search}${window.location.hash}`
-    ),
-  postLogin: () =>
-    // @ts-ignore
-    window.location.replace(localStorage.getItem('preLoginPath')),
-}
-
-const Entrypoint = () => {
-  if (!authEnabled) {
-    return <App />
-  }
-  return (
-    <AuthProvider authConfig={authConfig}>
-      <App />
-    </AuthProvider>
-  )
-}
-
-ReactDOM.render(<Entrypoint />, document.getElementById('app'))
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+)
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
