f4024bf2c5edc78b26c50497e34d35170461bf53
Olava Faksdal <38139899+olavis@users.noreply.github.com>
2022-06-28 10:52:18 +0200

    refactor: remove empty catch

--- web/src/components/TodoApp.tsx
+++ web/src/components/TodoApp.tsx
@@ -59,7 +59,6 @@ export const TodoApp = () => {
     todoAPI
       .getAll()
       .then((response) => setTodos(response.data))
-      .catch((e) => {})
   }, [])
 
   const addItem = (event: any) => {
