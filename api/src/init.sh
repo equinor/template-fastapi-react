d95667857ab33725fb4480ad0446fa3dad4019c4
Eirik Ola Aksnes <eoaksnes@gmail.com>
2022-11-01 16:07:45 +0100

    refactor: use version from pyproject.toml in docs

--- api/src/init.sh
+++ api/src/init.sh
@@ -5,7 +5,7 @@ if [ "$1" = 'api' ]; then
   if [ "${ENVIRONMENT:-'local'}" != "local" ]; then
     gunicorn app:create_app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:5000
   else
-    python3 /code/app.py run
+    python3 ./app.py run
   fi
 else
   exec "$@"
