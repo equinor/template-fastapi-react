e8304a3c143bb8351fbea510f71f4ed725cbb60f
Eirik Ola Aksnes <eoaksnes@gmail.com>
2022-11-03 17:09:54 +0100

    docs: addd list of features

--- documentation/src/pages/index.tsx
+++ documentation/src/pages/index.tsx
@@ -3,42 +3,78 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-
 import styles from './index.module.css';
 
+const features = [
+    {title: "Auto-generated changelogs from Git history"},
+    {title: "Auto-generated API documentations (using FastAPI)"},
+    {title: "Auto-generated API clients (using openapi generator)"},
+    {title: "Pre-commit hooks"},
+    {title: "CI/CD (using Github Actions)"},
+    {title: "Pydantic data validation"},
+    {title: "OAuth2"},
+    {title: "Standardized API error and response model"},
+    {title: "Run using Docker"},
+    {title: "Documentation solution (using Docusaurus)"},
+]
+
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/about/introduction">
-            Start reading the docs
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
+    const {siteConfig} = useDocusaurusContext();
+    return (
+        <header className={clsx('hero hero--primary', styles.heroBanner)}>
+            <div className="container">
+                <h1 className="hero__title">{siteConfig.title}</h1>
+                <p className="hero__subtitle">{siteConfig.tagline}</p>
+                <div className={styles.buttons}>
+                    <Link
+                        className="button button--secondary button--lg"
+                        to="/docs/about/introduction"
+                    >
+                        Start reading the docs
+                    </Link>
+                </div>
+            </div>
+        </header>
+    );
 }
 
-export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      title={`Frontpage | ${siteConfig.title}`}
-      description={`Frontpage for ${siteConfig.title}`}>
-      <HomepageHeader />
-      <main>
-        <div className="container">
-          <p className={styles.description}>
-            Template FastAPI React is a solution template for creating a Single Page App (SPA) with React and FastAPI following the principles of Clean Architecture.
-          </p>
+function Feature({title}) {
+    return (
+        <div className={clsx(styles.glass)}>
+            <div className={clsx(styles.feature)}>
+                {title}
+            </div>
         </div>
-      </main>
-    </Layout>
-  );
+    );
+}
+
+
+export default function Home(): JSX.Element {
+    const {siteConfig} = useDocusaurusContext();
+    return (
+        <Layout
+            title={`Frontpage | ${siteConfig.title}`}
+            description={`Frontpage for ${siteConfig.title}`}
+        >
+            <HomepageHeader/>
+            <main>
+                <div className="container">
+                    <p className={styles.description}>
+                        Things included
+                    </p>
+                </div>
+                {features && features.length > 0 && (
+                    <section className={styles.features}>
+                        <div className="container">
+                            <div className="row">
+                                {features.map((props, idx) => (
+                                    <Feature key={idx} {...props} />
+                                ))}
+                            </div>
+                        </div>
+                    </section>
+                )}
+            </main>
+        </Layout>
+    );
 }
