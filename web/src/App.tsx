51c9870bc657d586464780c664aeb6edb49b5ba2
Sebastian Vittersø <svit@equinor.com>
2022-11-08 14:49:33 +0100

    feat: expand and visualize auth states

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,47 @@
 import { router } from './router'
 import { RouterProvider } from 'react-router-dom'
 import Header from './common/components/Header'
+import { AuthContext } from 'react-oauth2-code-pkce'
+import { useContext } from 'react'
+import { Button, Progress, Typography } from '@equinor/eds-core-react'
+import styled from 'styled-components'
+
+const hasAuthConfig = process.env.REACT_APP_AUTH === '1'
+
+const CenterContainer = styled.div`
+  display: flex;
+  gap: 12px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+`
 
 function App() {
+  const { token, error, login, loginInProgress } = useContext(AuthContext)
+
+  if (hasAuthConfig && error) {
+    return <CenterContainer>{error}</CenterContainer>
+  }
+
+  if (hasAuthConfig && loginInProgress) {
+    return (
+      <CenterContainer>
+        <Typography>Login in progress.</Typography>
+        <Progress.Dots color="primary" />
+      </CenterContainer>
+    )
+  }
+
+  if (hasAuthConfig && !token) {
+    return (
+      <CenterContainer>
+        <Button onClick={login}>Log in</Button>
+      </CenterContainer>
+    )
+  }
+
   return (
     <>
       <Header />
--- web/src/auth.ts
+++ web/src/auth.ts
@@ -1,10 +1,13 @@
-export const authConfig = {
+import { TAuthConfig } from 'react-oauth2-code-pkce'
+
+export const authConfig: TAuthConfig = {
   clientId: process.env.REACT_APP_AUTH_CLIENT_ID || '',
   authorizationEndpoint: process.env.REACT_APP_AUTH_ENDPOINT || '',
   tokenEndpoint: process.env.REACT_APP_TOKEN_ENDPOINT || '',
   scope: process.env.REACT_APP_AUTH_SCOPE || '',
   redirectUri: process.env.REACT_APP_AUTH_REDIRECT_URI || '',
   logoutEndpoint: process.env.REACT_APP_LOGOUT_ENDPOINT || '',
+  autoLogin: false,
   preLogin: () =>
     localStorage.setItem(
       'preLoginPath',
