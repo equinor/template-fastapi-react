613cc4257699ddb1c2a772b54f28ccec84f2778b
Sebastian Vittersø <svit@equinor.com>
2022-11-10 15:12:12 +0100

    fix(api): raise MissingPrivilegeException when relevant

--- api/src/features/todo/use_cases/delete_todo_by_id.py
+++ api/src/features/todo/use_cases/delete_todo_by_id.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel, Field
 
-from common.exceptions import NotFoundException
+from common.exceptions import MissingPrivilegeException, NotFoundException
 from data_providers.repository_interfaces.TodoRepositoryInterface import (
     TodoRepositoryInterface,
 )
@@ -12,7 +12,9 @@ class DeleteTodoByIdResponse(BaseModel):
 
 def delete_todo_use_case(id: str, user_id: str, todo_repository: TodoRepositoryInterface) -> DeleteTodoByIdResponse:
     todo_item = todo_repository.get(id)
-    if todo_item is None or todo_item.user_id != user_id:
+    if todo_item is None:
         raise NotFoundException
+    if todo_item.user_id != user_id:
+        raise MissingPrivilegeException
     todo_repository.delete(id)
     return DeleteTodoByIdResponse(success=True)
