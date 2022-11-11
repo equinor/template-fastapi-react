31da3f7e720d0838e59e280aa4d873b44e24cecb
Olava Faksdal <38139899+olavis@users.noreply.github.com>
2022-11-11 10:42:48 +0100

    fix: test suite

--- web/src/App.test.tsx
+++ web/src/App.test.tsx
@@ -1,9 +1,6 @@
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import App from './App'
 
-test('renders learn react link', () => {
+test('renders without crashing', () => {
   render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  // @ts-ignore
-  expect(linkElement).toBeInTheDocument()
 })
