f1f871d41de45a8cde613c910c55854538b2b8e0
Jørgen <jenge@equinor.com>
2023-01-16 14:18:22 +0100

    fix(api): make health-check return PlainTextResponse

--- api/src/features/health_check/health_check_feature.py
+++ api/src/features/health_check/health_check_feature.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter, status
+from fastapi.responses import PlainTextResponse
 
 router = APIRouter(tags=["health_check"], prefix="/health-check")
 
 
 @router.get(
     "",
-    status_code=status.HTTP_200_OK,
-    responses={status.HTTP_200_OK: {"model": str, "content": {"application/json": {"example": "OK"}}}},
+    responses={status.HTTP_200_OK: {"model": str, "content": {"plain/text": {"example": "OK"}}}},
 )
-async def get() -> str:
-    return "OK"
+async def get() -> PlainTextResponse:
+    return PlainTextResponse("OK")
