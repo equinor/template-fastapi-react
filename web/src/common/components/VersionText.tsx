cc33a5d1f856aff8e529c4c3df43d326fb5a8a90
Olava Faksdal <38139899+olavis@users.noreply.github.com>
2023-02-27 09:41:34 +0100

    refactor: separate responsibilities by moving useEffect to a custom hook

--- web/src/common/components/VersionText.tsx
+++ web/src/common/components/VersionText.tsx
@@ -1,8 +1,8 @@
 import { Typography } from '@equinor/eds-core-react'
-import { useEffect, useState } from 'react'
 import axios, { AxiosResponse } from 'axios'
+import { useEffect, useState } from 'react'
 
-export const VersionText = (): JSX.Element => {
+const useCommitInfo = () => {
   const [commitInfo, setCommitInfo] = useState<{ [key: string]: string }>({
     hash: '',
     date: '',
@@ -14,14 +14,18 @@ export const VersionText = (): JSX.Element => {
       .get('version.txt')
       .then((response: AxiosResponse<string>) => {
         const versionFile: { [key: string]: string } = Object.fromEntries(
-          response.data.split('\n').map((line) => {
-            return line.split(': ')
-          })
+          response.data.split('\n').map((line) => line.split(': '))
         )
         setCommitInfo(versionFile)
       })
       .catch(() => null)
   }, [])
+
+  return commitInfo
+}
+
+export const VersionText = (): JSX.Element => {
+  const commitInfo = useCommitInfo()
 
   return (
     <p>
