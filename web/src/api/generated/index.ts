afa8158c9815352de7195d6d988302a6d7d87ffb
Peter Bull Hove <peterbullhove@gmail.com>
2023-08-30 16:12:59 +0200

    fix: ran api again

---
+++ web/src/api/generated/index.ts
@@ -0,0 +1,22 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export { ApiError } from './core/ApiError';
+export { CancelablePromise, CancelError } from './core/CancelablePromise';
+export { OpenAPI } from './core/OpenAPI';
+export type { OpenAPIConfig } from './core/OpenAPI';
+
+export { AccessLevel } from './models/AccessLevel';
+export type { AddTodoRequest } from './models/AddTodoRequest';
+export type { AddTodoResponse } from './models/AddTodoResponse';
+export type { DeleteTodoByIdResponse } from './models/DeleteTodoByIdResponse';
+export type { ErrorResponse } from './models/ErrorResponse';
+export type { GetTodoByIdResponse } from './models/GetTodoByIdResponse';
+export type { UpdateTodoRequest } from './models/UpdateTodoRequest';
+export type { UpdateTodoResponse } from './models/UpdateTodoResponse';
+export type { User } from './models/User';
+
+export { HealthCheckService } from './services/HealthCheckService';
+export { TodosService } from './services/TodosService';
+export { WhoamiService } from './services/WhoamiService';
---
+++ web/src/api/generated/models/ErrorResponse.ts
@@ -0,0 +1,13 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type ErrorResponse = {
+    status?: number;
+    type?: string;
+    message?: string;
+    debug?: string;
+    extra?: (Record<string, any> | null);
+};
+
