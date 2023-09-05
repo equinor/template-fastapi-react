81fde9c2e0726f7426c938a6877014c84a1d271f
Ingerid Gjeitnes Hellen <ingerid.hellen@bouvet.no>
2023-09-05 14:44:41 +0200

    test: add some simple web tests

--- web/src/App.test.tsx
+++ web/src/App.test.tsx
@@ -1,6 +1,14 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import App from './App'
 
 test('renders without crashing', () => {
   render(<App />)
+})
+test('has an input field', () => {
+  render(<App />)
+  expect(screen.getByPlaceholderText('Add Task')).toBeDefined()
+})
+test('has an Add button', () => {
+  render(<App />)
+  expect(screen.getByText('Add')).toBeDefined()
 })
