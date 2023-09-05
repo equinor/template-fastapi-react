a2fea020e0669a4cda3e324f47eba218d5b67d8e
Ingerid Gjeitnes Hellen <ingerid.hellen@bouvet.no>
2023-09-05 14:26:48 +0200

    chore: generate api with axios

--- web/generate-api-typescript-client.sh
+++ web/generate-api-typescript-client.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # This requires the API to be running on localhost port 5000
-yarn openapi -i http://localhost:5000/openapi.json -o "$PWD/src/api/generated"
+yarn openapi -i http://localhost:5000/openapi.json -o "$PWD/src/api/generated" -c "axios"
 
