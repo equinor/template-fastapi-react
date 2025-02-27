e0a599f6cd7ac22c3bf3094ef09a86a220b5f726
Sebastian Vittersø <svit@equinor.com>
2025-02-27 08:47:26 +0100

    refactor: replace `promise.then` syntax with `await`

--- web/src/hooks/useTodoAPI.tsx
+++ web/src/hooks/useTodoAPI.tsx
@@ -3,42 +3,50 @@ import { type AddTodoResponse, ApiError, type ErrorResponse, TodoService } from 
 
 export function useTodoAPI() {
   const addTodoItem = useCallback(async (title: string) => {
-    return TodoService.create({ title }).catch((error) => {
+    try {
+      return await TodoService.create({ title })
+    } catch (error) {
       if (error instanceof ApiError) {
         console.error((error.body as ErrorResponse).message)
       }
       throw error
-    })
+    }
   }, [])
 
   const getAllTodoItems = useCallback(async () => {
-    return TodoService.getAll().catch((error) => {
+    try {
+      return await TodoService.getAll()
+    } catch (error) {
       if (error instanceof ApiError) {
         console.error((error.body as ErrorResponse).message)
       }
       throw error
-    })
+    }
   }, [])
 
   const toggleTodoItem = useCallback(async (todo: AddTodoResponse) => {
-    return TodoService.updateById(todo.id, {
-      is_completed: !todo.is_completed,
-      title: todo.title,
-    }).catch((error) => {
+    try {
+      return await TodoService.updateById(todo.id, {
+        is_completed: !todo.is_completed,
+        title: todo.title,
+      })
+    } catch (error) {
       if (error instanceof ApiError) {
         console.error((error.body as ErrorResponse).message)
       }
       throw error
-    })
+    }
   }, [])
 
   const removeTodoItem = useCallback(async (todo: AddTodoResponse) => {
-    return TodoService.deleteById(todo.id).catch((error) => {
+    try {
+      return await TodoService.deleteById(todo.id)
+    } catch (error) {
       if (error instanceof ApiError) {
         console.error((error.body as ErrorResponse).message)
       }
       throw error
-    })
+    }
   }, [])
 
   return { addTodoItem, getAllTodoItems, toggleTodoItem, removeTodoItem }
