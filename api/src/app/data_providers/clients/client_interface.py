5c7dc37e2ef67149b5d78ebb6d6d2e467c11d012
Sindre Nistad <snis@equinor.com>
2026-03-06 15:57:45 +0100

    chore: Update `pre-commit` hooks
    Since we now use Python 3.14 with ruff, we may use newer syntax for the client.
    
    The `ruff-check` failed due to the use of `Generic`

--- api/src/app/data_providers/clients/client_interface.py
+++ api/src/app/data_providers/clients/client_interface.py
@@ -1,39 +1,33 @@
-from abc import abstractmethod
-from typing import Any, Generic, TypeVar
-
-# Type definition for Model
-M = TypeVar("M")
-
-# Type definition for Unique Id
-K = TypeVar("K")
+from abc import ABCMeta, abstractmethod
+from typing import Any
 
 # Type definition for filter
-FilterDict = dict[str, Any]
+type FilterDict = dict[str, Any]
 
 
-class ClientInterface(Generic[M, K]):
+class ClientInterface[Model, ID](metaclass=ABCMeta):
     @abstractmethod
-    def create(self, instance: M) -> M:
+    def create(self, instance: Model) -> Model:
         pass
 
     @abstractmethod
-    def delete(self, id: K) -> bool:
+    def delete(self, id: ID) -> bool:
         pass
 
     @abstractmethod
-    def get(self, id: K) -> M:
+    def get(self, id: ID) -> Model:
         pass
 
     @abstractmethod
-    def list_collection(self) -> list[M]:
+    def list_collection(self) -> list[Model]:
         pass
 
     @abstractmethod
-    def update(self, id: K, instance: M) -> M:
+    def update(self, id: ID, instance: Model) -> Model:
         pass
 
     @abstractmethod
-    def insert_many(self, instances: list[M]) -> None:
+    def insert_many(self, instances: list[Model]) -> None:
         pass
 
     @abstractmethod
@@ -41,11 +35,11 @@ class ClientInterface(Generic[M, K]):
         pass
 
     @abstractmethod
-    def find(self, filter: FilterDict) -> M:
+    def find(self, filter: FilterDict) -> Model:
         pass
 
     @abstractmethod
-    def find_one(self, filter: FilterDict) -> M | None:
+    def find_one(self, filter: FilterDict) -> Model | None:
         pass
 
     @abstractmethod
